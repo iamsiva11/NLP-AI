16a7d4a89c11af68fadf4951e5e1bdf8dfd35f6f
Siva Sundaramoorthy <siva11.sundaramoorthy@gmail.com>
2017-03-29 00:00:48 +0530

    to lower, combine text features

--- TextData-Prep/CleaningData.py
+++ TextData-Prep/CleaningData.py
@@ -1,7 +1,6 @@
 # Convert all to lowercase
 dataset['title'] = dataset['title'].map(lambda x: str(x).lower() )
-dataset['description'] = dataset['description'].map(lambda x: str(x).lower() )
-dataset['productBrand'] = dataset['productBrand'].map(lambda x: str(x).lower() )
-dataset['keySpecsStr'] = dataset['keySpecsStr'].map(lambda x: str(x).lower() )
-dataset['detailedSpecsStr'] = dataset['detailedSpecsStr'].map(lambda x: str(x).lower() )
-dataset['sellerName'] = dataset['sellerName'].map(lambda x: str(x).lower() )
+
+
+# Combine all features into one
+dataset['allTextFeaures'] = dataset['title'] + ' '  + dataset['description']
